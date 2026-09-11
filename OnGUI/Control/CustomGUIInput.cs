97b7d14b4909ba59827352b975a42293f4120ef9
MerliSheep <2986307175@qq.com>
2026-09-11 20:37:42 +0800

    g

---
+++ OnGUI/Control/CustomGUIInput.cs
@@ -0,0 +1,23 @@
+public class CustomGUIInput : CustomGUIControl
+{
+    public event UnityAction<string> textChange;
+    private string oldStr = "";
+    protected override void StyleOffDraw()
+    {
+        content.text = GUI.TextField(guiPos.Pos, content.text);
+        if(oldStr != content.text)
+        {
+            textChange?.Invoke(oldStr);
+            oldStr = content.text;
+        }
+    }
+    protected override void StyleOnDraw()
+    {
+        content.text = GUI.TextField(guiPos.Pos, content.text, style);
+        if (oldStr != content.text)
+        {
+            textChange?.Invoke(oldStr);
+            oldStr = content.text;
+        }
+    }
+}
